dcf70e2f95f5c0c6930461e4224ae9163e239778
è????? <1915250964@qq.com>
2016-10-07 00:08:56 +0800

    add footer  banner

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { NavComponent } from './nav/nav.component';
 import { CheckComponent } from './check/check.component';
 
+//todo  配置找不到path时，跳转到公共页面
 const routes: Routes = [
   { path: '', component: HomeComponent,canActivate:[CheckComponent] },
-  { path: 'about', component: AboutComponent,canActivate:[CheckComponent]},
-  { path: 'nav', component: NavComponent}
+  { path: 'about', component: AboutComponent,canActivate:[CheckComponent]}
 ];
 
 export const routing = RouterModule.forRoot(routes, { useHash: true });
