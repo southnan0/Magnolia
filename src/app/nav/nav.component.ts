8afc6d605eff3ea37a7c79fe21ab1d9d99d8d0fe
?¨????? <1915250964@qq.com>
2016-10-06 13:30:21 +0800

    add some project

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() {
+    // Do stuff
+  }
+
+  ngOnInit() {
+    console.log('Hello Nav');
+  }
+
+}
