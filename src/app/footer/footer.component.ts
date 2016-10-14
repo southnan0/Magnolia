82b09dcd5a9b3789025053a29b3bb3b822f49268
蜗牛 <1915250964@qq.com>
2016-10-15 00:27:06 +0800

    clean some code useless

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector:'my-footer',
     templateUrl:'./footer.component.html',
-    styleUrls:['../nav/nav.component.scss']
+    styleUrls:['./footer.component.scss']
 })
 export class FooterComponent implements OnInit{
     constructor(){}
